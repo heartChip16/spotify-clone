09306f5c48eacffdbbc03cef4f3e832e40fe36d7
Ingrid E. Torres <ingrid.escabal@gmail.com>
2022-11-24 10:18:26 +0800

    built for initial deployment

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,15 @@ export default defineConfig(({ command, mode }) => {
   // Load env file based on `mode` in the current working directory.
   // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
   const env = loadEnv(mode, process.cwd(), '')
+  // console.log("__dirname: ", __dirname);
+  // console.log("root: ", root);
+
   return {
     // vite config
     define: {
       __APP_ENV__: env.APP_ENV
     },
-    root: "src",
+    root: "./src",
     build: {
       outDir: "../dist",
       rollupOptions: {
@@ -18,6 +21,7 @@ export default defineConfig(({ command, mode }) => {
           main: resolve(__dirname, 'src/index.html'),
           dashboard: resolve(__dirname, 'src/dashboard/dashboard.html'),
           login: resolve(__dirname, 'src/login/login.html')
+
         }
       }
     }
