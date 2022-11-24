0c4bcce989096b42592033a77f9a1284da1dfdc4
Ingrid E. Torres <ingrid.escabal@gmail.com>
2022-11-24 11:24:27 +0800

    some changes

--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
 import './style.css'
 const APP_URL = import.meta.env.VITE_APP_URL;
 
-document.addEventListener("DOMContentLoaded", ()=>{
-    if(localStorage.getItem("accessToken") !==null){
-      // window.location.href = "dashboard/dashboard.html";
-      window.location.href = `${APP_URL}/dashboard/dashboard.html`;
-    }else {
-      // window.location.href = "login/login.html";
-      window.location.href = `${APP_URL}/login/login.html`;
-    }
+document.addEventListener("DOMContentLoaded", () => {
+  if (localStorage.getItem("accessToken") !== null) {
+    // window.location.href = "dashboard/dashboard.html";
+    window.location.href = `${APP_URL}/dashboard/dashboard.html`;
+  } else {
+    // window.location.href = "login/login.html";
+    window.location.href = `${APP_URL}/login/login.html`;
+  }
 })
 
